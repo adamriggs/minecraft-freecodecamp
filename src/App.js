0931c8da522f8ff8cf6b0db1943672621b4efbea
Adam R <phlux1111@gmail.com>
2022-11-19 08:29:42 -0500

    player done for now, onto using keyboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Physics } from '@react-three/cannon';
 import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { AmbientLight } from 'three';
 import { Ground } from './components/Ground';
+import { Player } from './components/Player';
+
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 	  	<Sky sunPosition={ [100, 100, 20] } />
 		<ambientLight intensity={0.5} />
 		<Physics>
+			<Player />
 			<Ground />
 		</Physics>
       </Canvas>
