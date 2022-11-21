67c2249cdae8ab528a83466950372bda1f081b8f
Adam R <phlux1111@gmail.com>
2022-11-21 07:12:00 -0500

    adding and removing cubes

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -3,18 +3,7 @@ import { nanoid } from 'nanoid';
 
 export const useStore = create((set)=> ({
 	texture: 'dirt',
-	cubes: [
-		{
-			key: nanoid(),
-			pos: [2, 0.5, 0],
-			texture: 'dirt',
-		},
-		{
-			key: nanoid(),
-			pos: [1, 0.5, 0],
-			texture: 'wood',
-		},
-	],
+	cubes: [],
 	addCube: (x,y,z) => {
 		set((prev)=>({
 			cubes: [
@@ -27,7 +16,15 @@ export const useStore = create((set)=> ({
 			]
 		}));
 	},
-	removeCubes: () => {},
+	removeCube: (x, y, z) => {
+		set((prev) => ({
+			cubes: prev.cubes.filter(cube => {
+				const [X, Y, Z] = cube.pos
+				return X !== x || Y !== y || Z !== z
+			})
+
+		}))
+	},
 	setTexture: () => {},
 	saveWorld: () => {},
 	resetWorld: () => {},
